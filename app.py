b757254f75e6165c687d997b3385dfef56d47225
Ayush18457 <arbdch263@gmail.com>
2025-09-16 21:42:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,51 +2,43 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-# Function to get recommendations
 def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+    """Recommends 5 movies based on the selected movie."""
+    try:
+        movie_index = movies[movies['title'] == movie].index[0]
+        distances = sorted(list(enumerate(similarity[movie_index])), reverse=True, key=lambda x: x[1])[1:6]
+        
+        recommended_movies = []
+        for i in distances:
+            recommended_movies.append(movies.iloc[i[0]].title)
+        return recommended_movies
+    except (IndexError, KeyError):
+        return []
 
-    return recommended_movie_names,recommended_movie_posters
+# --- Main App ---
+st.title('Movie Recommender System')
+
+# Load the data files
+try:
+    movies = pd.DataFrame(pickle.load(open('model.pkl', 'rb')))
+    similarity = pickle.load(open('similarity.pkl', 'rb'))
+except FileNotFoundError:
+    st.error("Error: Make sure 'movies.pkl' and 'similarity.pkl' are in the same folder.")
+    st.stop()
 
 
-st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
-
-movie_list = movies['title'].values
+# Create a dropdown for movie selection
 selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
+    "Type or select a movie to get a recommendation",
+    movies['title'].values
 )
 
+# Button to show recommendations
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-
-
-
-
+    recommendations = recommend(selected_movie)
+    if recommendations:
+        st.subheader('Recommended for you:')
+        for movie_title in recommendations:
+            st.write(movie_title)
+    else:
+        st.warning("Could not find recommendations for the selected movie.")
